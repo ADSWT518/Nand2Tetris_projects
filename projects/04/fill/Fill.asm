7ad84e020f112ea5b381626305a2c2a5a8e8d04d
ADSWT518 <tangyazhou518@outlook.com>
2020-08-13 00:02:32 +0800

    finish project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// 
+
+
+(LOOP1)
+@KBD
+D = M
+@BLACKEN_THE_SCREEN
+D; JNE
+@CLEAR_THE_SCREEN
+D; JEQ
+@LOOP1
+0; JMP
+
+(BLACKEN_THE_SCREEN)
+@SCREEN
+D = A
+@address
+M = D // address = 16384
+@i
+M = 0
+
+(LOOP2)
+@i
+D = M
+@8191 // the whole address is 8192
+D = D - A
+@LOOP1
+D; JGT
+
+@address
+A = M
+M = -1
+
+@i
+M = M + 1
+@address
+M = M + 1 // address++
+@LOOP2
+0; JMP
+
+(CLEAR_THE_SCREEN)
+@SCREEN
+D = A
+@address
+M = D // address = 16384
+@i
+M = 0
+
+(LOOP3)
+@i
+D = M
+@8191
+D = D - A
+@LOOP1
+D; JGT
+
+@address
+A = M
+M = 0
+
+@i
+M = M + 1
+@address
+M = M + 1
+@LOOP3
+0; JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// int a = R0;
+// int b = R1;
+// int i = 1;
+// int product = 0;
+// while (i <= b) {
+//     product += a;
+//     i++;
+// }
+
+@i
+M = 1 // i = 1
+@R2
+M = 0 // product = 0
+@R0
+D = M
+@a
+M = D // a = RAM[0]
+@R1
+D = M
+@b
+M = D // b = RAM[1]
+
+(LOOP)
+@i
+D = M
+@b
+D = D - M
+@END
+D; JGT
+
+@a
+D = M
+@R2
+M = M + D
+@i
+M = M + 1
+@LOOP
+0; JMP
+
+(END)
+@END
+0; JMP
